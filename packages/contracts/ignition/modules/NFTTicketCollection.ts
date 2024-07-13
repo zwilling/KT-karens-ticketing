65196e193e385474c3ec93d77e68c2917a87e719
Frederik Zwilling <frederik.zwilling@gmail.com>
2024-07-13 14:28:26 +0200

    contracts: updated deployment with event date

--- packages/contracts/ignition/modules/NFTTicketCollection.ts
+++ packages/contracts/ignition/modules/NFTTicketCollection.ts
@@ -1,16 +1,19 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const NFTTicketCollectionModule = buildModule("NFTTicketCollection", (m) => {
-  const uri = m.getParameter("uri", "https://raw.githubusercontent.com/zwilling/karen/main/packages/metadata/ethereumFilm/0.jpg");
+  const uri = m.getParameter("uri", "https://raw.githubusercontent.com/zwilling/karen/main/packages/metadata/ethereumFilm/0.json");
   const ticketID = m.getParameter("ticketID", 0);
   const initialAmount = m.getParameter("amount", 10);
+
+  const eventStart = m.getParameter("eventStart", 1731351600); // Nov 11 2024
+
   // const feeLoadValue = m.getParameter("lockedAmount", ONE_GWEI);
 
   const deployer = m.getAccount(0);
   const organizer = m.getAccount(1);
   const buyer = m.getAccount(2);
 
-  const nftTicketCollection = m.contract("NFTTicketCollection", [uri], {
+  const nftTicketCollection = m.contract("NFTTicketCollection", [uri, eventStart], {
     from: organizer,
     // value: lockedAmount,
   });
