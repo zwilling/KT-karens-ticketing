1fecf624eeb28df5e635e9dd8f734638e989d9bc
mpascualach <mpascualacheson1@gmail.com>
2024-07-13 16:57:32 +0200

    removed original nextjs project

--- apps/platform/karen-frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.karen.app',
-  appName: 'karen-frontend',
-  webDir: 'public'
-};
-
-export default config;
