f6257ebe67a69500ae0f5b6131e5f3a075cc68c3
Frederik Zwilling <frederik.zwilling@gmail.com>
2024-07-13 18:56:23 +0200

    api: example to get NFT metadata

---
+++ apps/opensea-api-test/get-ticket-data.ts
@@ -0,0 +1,61 @@
+import opensea from '@api/opensea';
+
+opensea.server('https://testnets-api.opensea.io');
+opensea.get_nft({
+  chain: 'base_sepolia',
+  address: '0x2aeaff494abf43bcd7fefba74794e0517edff901',
+  identifier: '0'
+})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+
+
+const resultExample =
+{
+  "nft": {
+    "identifier": "0",
+    "collection": "unidentified-contract-61febcce-f411-421b-969c-0cf6",
+    "contract": "0x2aeaff494abf43bcd7fefba74794e0517edff901",
+    "token_standard": "erc1155",
+    "name": "VITALIK: AN ETHEREUM STORY",
+    "description": "Cinema tickets for the movie 'VITALIK: AN ETHEREUM STORY'. Taking place in 'The EGG', Brussels, Belgium",
+    "image_url": "https://raw.githubusercontent.com/zwilling/karen/main/packages/metadata/ethereumFilm/image.jpg",
+    "display_image_url": "https://i.seadn.io/s/raw/files/73f4a8ee4fd5a75f7846bfe354da79d3.jpg?w=500&auto=format",
+    "display_animation_url": null,
+    "metadata_url": "https://raw.githubusercontent.com/zwilling/karen/main/packages/metadata/ethereumFilm/0.json",
+    "opensea_url": "https://testnets.opensea.io/assets/base_sepolia/0x2aeaff494abf43bcd7fefba74794e0517edff901/0",
+    "updated_at": "2024-07-13T12:28:08.715369",
+    "is_disabled": false,
+    "is_nsfw": false,
+    "animation_url": null,
+    "is_suspicious": false,
+    "creator": "0xcaadd2b763f5be6cd5baea3135c5b86e8af2eb06",
+    "traits": [
+      {
+        "trait_type": "Venue",
+        "display_type": null,
+        "max_value": null,
+        "value": "The EGG, Brussels, Belgium"
+      },
+      {
+        "trait_type": "Event Name",
+        "display_type": null,
+        "max_value": null,
+        "value": "VITALIK: AN ETHEREUM STORY"
+      },
+      {
+        "trait_type": "Date",
+        "display_type": "date",
+        "max_value": null,
+        "value": 1731351600
+      }
+    ],
+    "owners": [
+      {
+        "address": "0xcaadd2b763f5be6cd5baea3135c5b86e8af2eb06",
+        "quantity": 10
+      }
+    ],
+    "rarity": null
+  }
+}
