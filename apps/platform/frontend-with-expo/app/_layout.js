d479a4f1522046f95c729718932c95be83fbb18f
mpascualach <mpascualacheson1@gmail.com>
2024-07-14 01:04:28 +0200

    added loading + tried backwards navigation

--- apps/platform/frontend-with-expo/app/_layout.js
+++ apps/platform/frontend-with-expo/app/_layout.js
@@ -1,18 +1,19 @@
 import Header from "../components/header";
-import { View, StyleSheet } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View } from "react-native";
 import { Slot } from "expo-router";
 import "../styles/global.css";
 
+import { NavigationProvider } from '../contexts/NavigationContext';
+
 
 export default function Layout() {
-  const Stack = createStackNavigator();
   
   return (
-    <View style={[ { height: "100vh", width: "100%", background: 'linear-gradient(to bottom, #FFCDCD, #C6DFCA)' } ]}>
-      <Header></Header>
-      <Slot />
-    </View>
+    <NavigationProvider>
+      <View style={[ { height: "100vh", width: "100%", background: 'linear-gradient(to bottom, #FFCDCD, #C6DFCA)' } ]}>
+        <Header></Header>
+        <Slot />
+      </View>
+    </NavigationProvider>
   )
 }
